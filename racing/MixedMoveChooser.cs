57c47c24dd44ee04f9549a595efbfb2996286437
Zeliboba <tolya12345w@gmail.com>
2020-04-15 11:39:31 +0500

    Added mixed move chooser

---
+++ racing/MixedMoveChooser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AiAlgorithms.Algorithms;
+
+namespace AiAlgorithms.racing
+{
+    public class MixedMoveChooser : IMoveChooser
+    {
+        private readonly List<IMoveChooser> Choosers = new List<IMoveChooser>();
+        public MixedMoveChooser(int depth = 20,
+            double flagsTakenC = 10000, double distC = 1, double nextFlagC = 1 / 4)
+        {
+            Choosers.Add(new GreedyMoveChooser(new SumWeighter(), depth, flagsTakenC, distC, nextFlagC));
+            Choosers.Add(new RandomMoveChooser(depth, flagsTakenC, distC, nextFlagC));
+        }
+        
+        public (ICarCommand FirstCarCommand, ICarCommand SecondCarCommand)[] GetCarCommands(V nextFlagForFirstCar,
+            V nextFlagForSecondCar, RaceState raceState, out string debugInfo)
+        {
+            var results = new List<(ICarCommand FirstCarCommand, ICarCommand SecondCarCommand)>();
+            debugInfo = "";
+            foreach (var chooser in Choosers){
+                results.AddRange(chooser.GetCarCommands(nextFlagForFirstCar, nextFlagForSecondCar, raceState, out var debug));
+                debugInfo += debug + "\n";
+            }
+            return results.ToArray();
+        }
+    }
+}
--- racing/RandomRacer.cs
+++ racing/RandomRacer.cs
@@ -5,77 +5,13 @@ using AiAlgorithms.Algorithms;
 
 namespace AiAlgorithms.racing
 {
-    public class RandomRacer : ISolver<RaceState, RaceSolution>
+    public class RandomRacer : AbstractRacer
     {
-        private static readonly ICarCommand[] Commands;
-        private readonly int depth;
-        private readonly EvaluationFunctions evaluationFunctions;
-        private PreviousBest firstPreviousBest = null;
-        private PreviousBest secondPreviousBest = null;
-
-        static RandomRacer()
+        public RandomRacer(int depth = 20,
+            double flagsTakenC = 10000, double distC = 1, double nextFlagC = 1 / 4)
         {
-            var list = new[] {0, 1, -1};
-            Commands = list
-                .SelectMany(t => list, (t1, t2) => (ICarCommand) new MoveCommand(new V(t1, t2)))
-                .Prepend(new ExchangeCommand())
-                .ToArray();
-        }
-
-        public RandomRacer(int depth = 20, double flagsTakenC = 10000, double distC = 1, double nextFlagC = 1 / 4)
-        {
-            this.depth = depth;
-            evaluationFunctions = new EvaluationFunctions(flagsTakenC, distC, nextFlagC);
-        }
-
-        public IEnumerable<RaceSolution> GetSolutions(RaceState problem, Countdown countdown)
-        {
-            var chooser = new MaxDistFlagChooser();
-            var (firstCarNextFlag, secondCarNextFlag) = chooser.GetNextFlagsFor(problem);
-            var firstCarResult = ChooseMoveForCar(true, problem, firstCarNextFlag);
-            var secondCarResult = ChooseMoveForCar(false, problem, secondCarNextFlag);
-            yield return new RaceSolution(new[]
-                {(firstCarResult, secondCarResult)});
-        }
-
-        private ICarCommand ChooseMoveForCar(bool ifFirstCar, RaceState problem, V thisFlag)
-        {
-            var rnd = new Random();
-            var resList = new List<(List<ICarCommand>, double, RaceState)>();
-            for (var i = 0; i < 10; i++)
-            {
-                var state = problem.MakeCopy();
-                var allCount = 0;
-                var evList = new List<double>();
-                var myCommands = new List<ICarCommand>();
-                while (allCount < depth)
-                {
-                    var pairInd = rnd.Next(10);
-                    var count = rnd.Next(3, 10);
-                    var command = Commands[pairInd];
-                    myCommands.Add(command);
-                    allCount += count;
-                    for (var j = 0; j < count; j++)
-                        evList.Add(evaluationFunctions.EvaluateCommand(state, ifFirstCar, thisFlag, command));
-                }
-
-                resList.Add((myCommands, evList.Max(), state));
-            }
-
-            var prevBest = ifFirstCar ? firstPreviousBest : secondPreviousBest;
-            if (prevBest != null)
-                foreach (var addingCommand in Commands)
-                {
-                    var addedList = new List<ICarCommand>(prevBest.CommandList) {addingCommand};
-                    prevBest.Score += evaluationFunctions.EvaluateCommand(prevBest.State,
-                        ifFirstCar, thisFlag, addingCommand);
-                    resList.Add((prevBest.CommandList, prevBest.Score, prevBest.State));
-                }
-
-            var res_V = resList.OrderByDescending(pair => pair.Item2).First();
-            var bestList = res_V.Item1.Skip(1).ToList();
-            prevBest = new PreviousBest(bestList, res_V.Item2, res_V.Item3);
-            return res_V.Item1.First();
+            FlagChooser = new MaxDistFlagChooser();
+            MoveChooser = new RandomMoveChooser(depth, flagsTakenC, distC, nextFlagC);
         }
     }
 }
